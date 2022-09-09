05202ba2bd0eed8d708459c3c909dfae2b75a742
bpunolaf <89323220+bpunolaf@users.noreply.github.com>
2022-09-09 03:06:35 -0300

    FinishApp

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { Article } from '../interfaces';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  private _storage: Storage | null = null;
+  private _localArticles:Article[] = [];
+
+
+  constructor(private storage:Storage) {
+    this.init();
+    
+
+   }
+
+   get getLocalArticles(){
+    return [...this._localArticles];
+  }
+
+   async init() {
+
+    const storage = await this.storage.create();
+    this._storage = storage;
+    this.loadFavorites();
+  }
+  
+  async saveOrRemoveArticle(article:Article){
+    const exist = this._localArticles.find(localArticle=>localArticle.title===article.title);
+
+    if(exist){
+      console.log("aca")
+      this._localArticles = this._localArticles.filter(localArticle=>localArticle.title!==article.title);
+    }else{
+
+      this._localArticles = [article,...this._localArticles];
+
+    }
+
+    this._storage.set('articles',this._localArticles);
+  }
+
+  async loadFavorites(){
+    try {
+      const articles = await this._storage.get('articles');
+      this._localArticles = articles || [];
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  articlesInFavorites(article:Article){
+    return !!this._localArticles.find(localArticle=>localArticle.title===article.title);
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiKey:'fbbb6962c39c4086863b300a2fb3ece6',
+  url:'https://newsapi.org/v2'
+};
